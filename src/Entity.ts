76c3e52085f945955012a3b13e23704e230be8db
Danny Rottstegge <danny@rottstegge.net>
2020-06-23 23:36:50 +0200

    First in

---
+++ src/Entity.ts
@@ -0,0 +1,8 @@
+export interface Entity {
+    $table: string;
+    _id?: string;
+    _rev?: string;
+    _deleted?: boolean;
+    created?: Date;
+    lastModified?: Date;
+}
---
+++ src/Repository.ts
@@ -0,0 +1,116 @@
+import PouchDB from 'pouchdb-core';
+import PouchFind from 'pouchdb-find';
+import { v4 as uuidv4 } from 'uuid';
+
+PouchDB.plugin(PouchFind);
+
+import { Entity } from './Entity';
+
+export interface RepositoryOptions {
+    db: PouchDB.Configuration.LocalDatabaseConfiguration;
+    deletedFlag?: boolean;
+}
+
+export type OnRepositoryChange<T> = { (data: T): void };
+
+export class Repository<T extends Entity> {
+    private readonly db: PouchDB.Database<T>;
+    private changeListeners: OnRepositoryChange<T>[] = [];
+
+    constructor(
+        private readonly table: string,
+        private readonly options: RepositoryOptions = { db: { name: 'db' }, deletedFlag: true }
+    ) {
+        this.table = table;
+        this.db = new PouchDB(options.db.name, options.db);
+    }
+
+    async save(item: T): Promise<T> {
+        const now = new Date();
+        if (this._isNew(item)) {
+            item._id = uuidv4();
+            item.created = now;
+        }
+        item.lastModified = now;
+        item = { ...item, $table: this.table };
+
+        const doc = await this.db.put(item);
+        item._id = doc.id;
+        item._rev = doc.rev;
+        return item;
+    }
+
+    async saveAll(items: T[]): Promise<T[]> {
+        let results: T[] = [];
+        items.forEach(async (i) => {
+            results.push(await this.save(i));
+        });
+        return results;
+    }
+
+    async get(id: string, options: PouchDB.Core.GetOptions = {}): Promise<T | undefined> {
+        try {
+            let result = await this.db.get(id, options);
+            return result;
+        } catch (e) {
+            return undefined;
+        }
+    }
+
+    async remove(item: string | T): Promise<void> {
+        let id = typeof item === 'string' ? item : item._id;
+        if (id === undefined) {
+            return;
+        }
+
+        if (this.options.deletedFlag) {
+            let entity = await this.get(id);
+            if (entity) {
+                entity._deleted = true;
+                await this.save(entity);
+            }
+        } else {
+            let doc = await this.db.get(id);
+            await this.db.remove(doc._id, doc._rev);
+        }
+    }
+
+    async removeAll(items: (string | T)[]): Promise<void> {
+        items.forEach(async (i) => {
+            await this.remove(i);
+        });
+    }
+
+    async query(options: PouchDB.Find.FindRequest<T> = { selector: { $table: this.table } }): Promise<T[]> {
+        let docs = await this.db.find(options);
+        return docs.docs as T[];
+    }
+
+    public onChange(handler: OnRepositoryChange<T>) {
+        this.changeListeners.push(handler);
+        this.db
+            .changes({
+                since: 'now',
+                live: true,
+                filter: (doc) => {
+                    return doc.$table === this.table;
+                },
+            })
+            .on('change', async (change) => {
+                const entity = await this.get(change.id);
+                if (entity) {
+                    this.changeListeners.forEach((listener) => {
+                        listener(entity);
+                    });
+                }
+            });
+    }
+
+    public removeOnChangeListener(handler: OnRepositoryChange<T>) {
+        this.changeListeners.filter((h) => h !== handler);
+    }
+
+    private _isNew(entity: T): boolean {
+        return entity._id === undefined;
+    }
+}
