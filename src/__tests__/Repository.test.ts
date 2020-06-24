e02b74f0ae0bcc2b4eb7d622c283a4c7e933e0b8
Danny Rottstegge <danny@rottstegge.net>
2020-06-24 09:17:06 +0200

    Add test

--- src/__tests__/Repository.test.ts
+++ src/__tests__/Repository.test.ts
@@ -14,7 +14,13 @@ class TestEntityRepository extends Repository<ITestEntity> {
 
 const repo = new TestEntityRepository();
 
-it('should save doc', async () => {
+it('should save entity', async () => {
     const entity = await repo.save({ name: 'John', age: 29 });
-    expect(entity).toBeTruthy();
+    expect(entity).not.toBeUndefined();
+    expect(entity).not.toBeNull();
+});
+
+it('should generate id', async () => {
+    const entity = await repo.save({ name: 'John', age: 29 });
+    expect(entity._id).toMatch(/^[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}$/);
 });
