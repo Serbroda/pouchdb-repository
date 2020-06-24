7025ef43d253d04351f2543e3d47f2ec60e8a3e9
Danny Rottstegge <danny@rottstegge.net>
2020-06-24 12:06:45 +0200

    Add test

--- src/__tests__/Repository.test.ts
+++ src/__tests__/Repository.test.ts
@@ -14,13 +14,18 @@ class TestEntityRepository extends Repository<ITestEntity> {
 
 const repo = new TestEntityRepository();
 const createEntity = (data?: ITestEntity) => {
-    const def: ITestEntity = { name: 'John', age: 29 };
-    return { ...def, ...data };
+    return { ...{ name: 'John', age: 29 }, ...data } as ITestEntity;
 };
 
 beforeEach(async () => {
     await repo.clear();
     repo.removeAllChangeListener();
+});
+
+it('should create repository', () => {
+    const repo1 = new TestEntityRepository();
+    expect(repo1).not.toBeUndefined();
+    expect(repo1).not.toBeNull();
 });
 
 it('should save entity', async () => {
@@ -93,6 +98,10 @@ it('should remove entity', async () => {
     let results = await repo.query();
     expect(results.length).toBe(4);
 
+    await repo.remove({});
+    results = await repo.query();
+    expect(results.length).toBe(4);
+
     await repo.remove(entity1);
     results = await repo.query();
     expect(results.length).toBe(3);
@@ -100,6 +109,21 @@ it('should remove entity', async () => {
     await repo.remove(entity2._id!);
     results = await repo.query();
     expect(results.length).toBe(2);
+
+    class TestEntityRepositoryWithNonDeletedFlag extends Repository<ITestEntity> {
+        constructor() {
+            super('test', {
+                db: { name: 'db' },
+                deletedFlag: false,
+            });
+        }
+    }
+
+    const repo2 = new TestEntityRepositoryWithNonDeletedFlag();
+
+    await repo2.remove({ _id: entity3._id });
+    results = await repo2.query();
+    expect(results.length).toBe(1);
 });
 
 it('should remove all given entities', async () => {
@@ -128,4 +152,50 @@ it('should listen for changes', (done: jest.DoneCallback) => {
     setTimeout(async () => {
         await repo.save(createEntity({ name: 'Anna' }));
     }, 200);
+});
+
+it('should not listen after listener removed', (done: jest.DoneCallback) => {
+    const callback = jest.fn();
+    repo.onChange(callback);
+    repo.removeOnChangeListener(callback);
+
+    setTimeout(async () => {
+        await repo.save(createEntity());
+        setTimeout(() => {
+            expect(callback).not.toHaveBeenCalled();
+            done();
+        }, 500);
+    }, 200);
+});
+
+it('should difference between multiple repositories', async () => {
+    class TestEntityRepository2 extends Repository<ITestEntity> {
+        constructor() {
+            super('test2');
+        }
+    }
+
+    const repo2 = new TestEntityRepository2();
+
+    await repo.saveAll([
+        createEntity({ name: 'Julia' }),
+        createEntity({ name: 'Hannah' }),
+        createEntity({ name: 'Mike' }),
+        createEntity({ name: 'Gerorge' }),
+        createEntity({ name: 'Iris' }),
+        createEntity({ name: 'Steve' }),
+    ]);
+    await repo2.saveAll([createEntity({ name: 'Peter' }), createEntity({ name: 'Iris' })]);
+
+    const result1 = await repo.query();
+    expect(result1.length).toBe(6);
+    expect(result1.filter((i) => i.name == 'Mike').length).toBe(1);
+    expect(result1.filter((i) => i.name == 'Peter').length).toBe(0);
+    expect((await repo.query({ selector: { name: 'Iris' } })).length).toBe(1);
+
+    const result2 = await repo2.query();
+    expect(result2.length).toBe(2);
+    expect(result2.filter((i) => i.name == 'Mike').length).toBe(0);
+    expect(result2.filter((i) => i.name == 'Peter').length).toBe(1);
+    expect((await repo2.query({ selector: { name: 'Iris' } })).length).toBe(1);
 });
