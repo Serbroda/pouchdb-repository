030b67bc15253eb0e9c26bf2f81aa019042539fe
Danny Rottstegge <danny@rottstegge.net>
2020-06-24 10:16:13 +0200

    Clear before each test

--- src/__tests__/Repository.test.ts
+++ src/__tests__/Repository.test.ts
@@ -18,7 +18,13 @@ const createEntity = (data?: ITestEntity) => {
     return { ...def, ...data };
 };
 
+beforeEach(async () => {
+    await repo.clear();
+});
+
 it('should save entity', async () => {
+    repo.clear();
+
     const entity = await repo.save(createEntity());
     expect(entity).not.toBeUndefined();
     expect(entity).not.toBeNull();
@@ -27,6 +33,13 @@ it('should save entity', async () => {
 it('should generate id', async () => {
     const entity = await repo.save(createEntity());
     expect(entity._id).toMatch(/^[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}$/);
+});
+
+it('should save all entities', async () => {
+    const entities = await repo.saveAll([createEntity(), createEntity()]);
+    expect(entities).not.toBeUndefined();
+    expect(entities).not.toBeNull();
+    expect(entities.length).toBe(2);
 });
 
 it('should get entity by id', async () => {
@@ -41,15 +54,43 @@ it('should query entities', async () => {
     await repo.save(createEntity({ name: 'Julia', age: 26 }));
     await repo.save(createEntity({ name: 'Emma', age: 26 }));
 
-    const results1 = await repo.query({ selector: { name: 'Julia' } });
+    const results1 = await repo.query();
     expect(results1).not.toBeUndefined();
     expect(results1).not.toBeNull();
-    expect(results1.length).toBe(1);
-    expect(results1[0].name).toBe('Julia');
+    expect(results1.length).toBe(3);
 
-    const results2 = await repo.query({ selector: { age: 26 } });
+    const results2 = await repo.query({ selector: { name: 'Julia' } });
     expect(results2).not.toBeUndefined();
     expect(results2).not.toBeNull();
-    expect(results2.length).toBe(2);
-    expect(results2[0].age).toBe(26);
+    expect(results2.length).toBe(1);
+    expect(results2[0].name).toBe('Julia');
+
+    const results3 = await repo.query({ selector: { age: 26 } });
+    expect(results3).not.toBeUndefined();
+    expect(results3).not.toBeNull();
+    expect(results3.length).toBe(2);
+    expect(results3[0].age).toBe(26);
+});
+
+it('should clear repository', async () => {
+    const entities = await repo.saveAll([createEntity(), createEntity()]);
+    expect(entities).not.toBeUndefined();
+    expect(entities).not.toBeNull();
+    expect(entities.length).toBe(2);
+
+    await repo.clear();
+    const results1 = await repo.query();
+    expect(results1).not.toBeUndefined();
+    expect(results1).not.toBeNull();
+    expect(results1.length).toBe(0);
+});
+
+it('should remove entity', async () => {
+    const entity1 = await repo.save(createEntity({ name: 'Max' }));
+    const entity2 = await repo.save(createEntity({ name: 'Julia' }));
+    const entity3 = await repo.save(createEntity({ name: 'Emma' }));
+    const entity4 = await repo.save(createEntity({ name: 'Florian' }));
+
+    const results = await repo.query();
+    expect(results.length).toBe(4);
 });
